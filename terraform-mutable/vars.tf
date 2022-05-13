4b4ac1ad756a6816dd8722cf2e49b9ba009f439f
priyankapeelu <priyankapeelu@icloud.com>
2022-05-13 15:49:12 +0530

    terraform variablescd tt

--- terraform-mutable/vars.tf
+++ terraform-mutable/vars.tf
@@ -6,15 +6,3 @@ variable "OD_INSTANCE_COUNT" {}
 variable "APP_PORT" {}
 variable "WORKSTATION_IP" {}
 variable "APP_VERSION" {}
-variable "APP_TYPE" {}
-
-
-
-
-#variable "INSTANCE_TYPE" {}
-#variable "SPOT_INSTANCE_COUNT" {}
-#variable "OD_INSTANCE_COUNT" {}
-#variable "APP_PORT" {}
-#variable "WORKSTATION_IP" {}
-#variable "APP_VERSION" {}
-#variable "APP_TYPE" {}
